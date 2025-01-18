b73100661e0e1efc6a3b1805fed8a4b98a140b64
shanvisoni <shanvisoni7388@gmail.com>
2025-01-18 19:39:15 +0530

    db.js correction

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,4 @@
-// const mongoose = require('mongoose');
-// const config = require('config');
-// const db = config.get('uri');
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(db);
-
-//     console.log('MongoDB Connected...');
-//   } catch (err) {
-//     console.error(err.message);
-//     // Exit process with failure
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
 
 
 const mongoose = require('mongoose');
@@ -24,9 +8,8 @@ const connectDB = async () => {
   console.log('MONGO_URI:', process.env.MONGO_URI); 
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: process.env.DB_NAME,  // Specify the database name here
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            dbName: process.env.DB_NAME  // Specify the database name here
+          
         });
         // console.log(`Connected to MongoDB Database at ${conn.connection.host}`.green);
     } catch (error) {
